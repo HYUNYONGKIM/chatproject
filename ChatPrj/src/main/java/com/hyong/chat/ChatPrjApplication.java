dbe9e77cc4957e193e16a1a6fa150d4b65ab13be
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 16:18:16 +0900

    프로젝트 생성

---
+++ ChatPrj/src/main/java/com/hyong/chat/ChatPrjApplication.java
@@ -0,0 +1,13 @@
+package com.hyong.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPrjApplication.class, args);
+	}
+
+}
---
+++ ChatPrj/src/test/java/com/hyong/chat/ChatPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyong.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
